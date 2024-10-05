815411901b4a6b3469deb54388daba386a220382
Francis Rombo <fromb@kali.>
2024-10-05 10:18:56 +0000

    defined the models and make migrations

---
+++ electionprj/election/dir.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+from django.db import models
+
+print(dir(models))
--- electionprj/election/models.py
+++ electionprj/election/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Voter(models.Model):
+    id = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    national_id = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    phone_number = models.IntegerField()
+    date_of_birth = models.DateField()
+    voted = models.BooleanField(default=False)
+
+
+class Candidate(models.Model):
+    id = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    voted = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    party = models.CharField(max_length=50)
+
+
+class Admin(models.Model):
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
